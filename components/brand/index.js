32f386708a0f842ab20ad1d7b067555129b1e641
huseyinbatta <huseyinbattal42@gmail.com>
2022-07-10 14:43:11 +0300

    logo

--- components/brand/index.js
+++ components/brand/index.js
@@ -1,21 +1,26 @@
 import Link from "next/link";
+const logo="https://reactdeveloper.site/react-logo.png"
 
 const Brand = () => {
   return (
     <>
       <div className="hidden xl:flex items-center items-stretch">
         <Link href="/" aria-label="Home Page">
-          <figure className="p-1 mr-2 cursor-pointer transition hover:scale-150 duration-500" >
-          <img width="40px" src="https://reactdeveloper.site/logobg.png" alt="HB Logo"/>
+          <figure className="p-1 mr-2 cursor-pointer transition hover:scale-150 duration-500">
+            <img
+              width="40px"
+              src="https://reactdeveloper.site/logobg.png"
+              alt="HB Logo"
+            />
           </figure>
         </Link>
 
         <div className="flex flex-col justify-between gap-1">
-          <strong className="text-xl leading-6 font-medium">
+          <strong className="text-bold text-2xl leading-6 font-medium text-red-400">
             Hüseyin BATTAL
           </strong>
-          <span className="text-xl font-light leading-none">
-            React Developer
+          <span className="text-2xl font-light leading-none flex gap-2 text-blue-400" >
+            React Developer <div className="text-red-400">{"{"}</div><img width="25px" src={logo} alt="logo" /> <div className="text-red-400">{"}"}</div>
           </span>
         </div>
       </div>
