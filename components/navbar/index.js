a74c42e5c31ce085d3cbbdf94c2b759f1395a71c
huseyinbatta <huseyinbattal42@gmail.com>
2022-07-10 15:25:53 +0300

    space

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = ({ data }) => {
   return (
     <>
-      <nav className="flex justify-center gap-x-7 gap-y-3 lg:gap-0 lg:space-x-7 -space-x-5 ">
+      <nav className="flex justify-center gap-x-7 gap-y-3 lg:gap-0 lg:space-x-7 -space-x-4 ">
         {data.items.map(({ title, link }, index) => (
           <Link href={link} key={index}>
             <a className="text-blue-500 rounded-xl p-2 border border-red-300 text-base lg:text-xl lg:leading-9 no-underline font-normal focus:bg-yellow-300 focus:rounded-xl">
