1c31abde14dc26c4244c59819bf19314dcee9fe4
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-29 17:51:31 +0300

    color

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -7,7 +7,7 @@ const Navbar = ({ data }) => {
       <nav className="flex justify-center gap-x-7 gap-y-3 lg:gap-0 lg:space-x-7 -space-x-5 ">
         {data.items.map(({ title, link }, index) => (
           <Link href={link} key={index}>
-            <a className="text-blue-500 rounded-xl p-2 border border-red-300 text-base lg:text-xl lg:leading-9 no-underline font-normal focus:bg-yellow-300 focus:rounded-xl">
+            <a className="text-blue-400 rounded-xl p-2 border border-green-500 text-base lg:text-xl lg:leading-9 no-underline font-normal hover:bg-green-100 hover:text-red-400 focus:bg-red-400 focus:rounded-xl focus:text-white">
               {title}
             </a>
           </Link>
