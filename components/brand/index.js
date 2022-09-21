83030fd67a6b60d9588420b088ab5c7e6451e713
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-21 18:10:10 +0300

    change navbar

--- components/brand/index.js
+++ components/brand/index.js
@@ -4,11 +4,11 @@ const logo="https://reactdeveloper.site/logo.svg"
 const Brand = () => {
   return (
     <>
-      <div className="hidden xl:flex items-center items-stretch">
+      <div className="hidden xl:flex items-center">
         <Link href="/" aria-label="Home Page">
           <figure className="p-1 mr-2 cursor-pointer transition hover:scale-150 duration-500">
             <img
-              width="40px"
+              width="50px"
               src="https://reactdeveloper.site/logobg.png"
               alt="HB Logo"
             />
@@ -16,9 +16,7 @@ const Brand = () => {
         </Link>
 
         <div className="flex flex-col justify-between gap-1">
-          <strong className="text-bold text-2xl leading-6 font-medium text-red-400">
-            Hüseyin BATTAL
-          </strong>
+        
           <span className="text-2xl font-light leading-none flex gap-2 text-blue-400" >
             React.JS Developer <span style={{color:"rgb(47,196,99)"}}>{"<"}</span><img width="25px"  src={logo} alt="logo" /><span style={{color:"rgb(47,196,99)"}}>{"/>"}</span>
           </span>
