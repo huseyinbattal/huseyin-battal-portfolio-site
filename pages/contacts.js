a37a133dc00fd1bea1db9607a2138a53e0ddc031
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-29 22:14:47 +0300

    change font-family and color.

--- pages/contacts.js
+++ pages/contacts.js
@@ -21,15 +21,15 @@ const Contacts = () => {
 
 
           <div className="lg:ml-10">
-            <h2>
-              <strong>Hello, I'm Hüseyin 👋</strong><br /><br />
-              <div className="float-left mr-6 mb-1">
+            <h1 className="font-serif text-center text-yellow-500">
+              <strong>Hello everyone, I'm Hüseyin 👋</strong><br /><br /></h1>
+              <div className="float-left mt-1 mr-6 mb-1 ">
               <img className="rounded-xl border-4 border-red-900" width="400px" src={hbsearch} />
               </div>
-              <h3 className="font-serif text-gray-500 md:text-4xl">I'm a software developer. I have been focusing on frontend technologies for the last few years. I'm developing frontend projects with react. I also have knowledge and experience about backend technologies. I love doing hobby projects and learning new technologies.</h3>
+              <h3 className="text-left font-serif text-gray-500 md:text-4xl">I'm a software developer. I have been focusing on frontend technologies for the last few years. I'm developing frontend projects with react. I also have knowledge and experience about backend technologies. I love doing hobby projects and learning new technologies.</h3>
               <br/>
               <h3 className="italic text-blue-600 text-sm lg:text-xl">You can contact me at the e-mail address below.👇</h3>
-            </h2>
+            
             <p className="italic text-sm lg:text-xl">
               <a
                 href="mailto:hsynbattal@icloud.com,"
