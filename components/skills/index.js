821db87d999446671634d8be957b1d34dd8cec3e
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-29 21:21:58 +0300

    change skills page style, add scale.

--- components/skills/index.js
+++ components/skills/index.js
@@ -27,7 +27,7 @@ const Skills = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-x-5 gap-y-12 mt-5">
           {arr.map((name, i) => (
             <figure
-              className="flex flex-col items-center justify-center h-28 hover:shadow-green-500 hover:shadow-xl cursor-pointer shadow-green-200 shadow-sm"
+              className="flex flex-col items-center justify-center h-28 hover:shadow-red-500 hover:shadow-xl cursor-pointer rounded-2xl shadow-green-200 shadow-sm active:shadow-red-500 active:shadow-xl hover:scale-125 duration-500"
               key={i}
             >
               <img
