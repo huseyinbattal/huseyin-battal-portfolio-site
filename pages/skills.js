cca58f40bb2030685a9bd6517d8805f9dfe751e4
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-29 23:54:58 +0300

    change page routes (about and skills)

---
+++ pages/skills.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import Title from "@/components/title"
+import Subtitle from "@/components/subtitle"
+import Skills from "@/components/skills"
+
+function About() {
+  return (
+    <>
+       <section className="mb-16 lg:mb-32">
+       <Title>My Skills</Title>
+
+        <Skills />
+      </section>
+
+
+
+
+
+
+     
+    </>
+  )
+}
+
+export default About
