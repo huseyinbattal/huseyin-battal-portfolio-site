fe54bb3d72db45d0a9670b73241709ef5953336a
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-30 16:31:13 +0300

    update

--- components/brand/index.js
+++ components/brand/index.js
@@ -4,9 +4,10 @@ const logo="https://reactdeveloper.site/giphy.gif"
 const Brand = () => {
   return (
     <>
-      <div className="hidden xl:flex items-center">
+      
+      <div className="flex justify-center items-center mb-4">
         <Link href="/" aria-label="Home Page">
-          <figure className="p-1 mr-2 cursor-pointer transition hover:scale-125 hover:rotate-180 duration-500">
+          <figure className="p-1 cursor-pointer transition hover:scale-125  duration-300">
             <img
               width="60px"
               src="https://reactdeveloper.site/logobg.png"
@@ -14,14 +15,14 @@ const Brand = () => {
             />
           </figure>
         </Link>
-
         <div className="flex flex-col justify-between gap-1">
         
           <span className="text-2xl font-serif leading-none flex gap-2 font-bold text-blue-400" >
-            React.JS Developer <span style={{color:"rgb(47,196,99)"}}>{"<"}</span><span className="hover:scale-150 duration-500"><img  width="30px"  src={logo} alt="logo" /></span><span style={{color:"rgb(47,196,99)"}}>{"/>"}</span>
+            React.JS Developer <span style={{color:"rgb(47,196,99)"}}>{"<"}</span><span className="cursor-pointer hover:scale-150 duration-500"><img  width="30px"  src={logo} alt="logo" /></span><span style={{color:"rgb(47,196,99)"}}>{"/>"}</span>
           </span>
         </div>
       </div>
+    
     </>
   );
 };
