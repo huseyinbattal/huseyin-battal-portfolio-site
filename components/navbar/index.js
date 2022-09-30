ac27e1613aac54a50bd82da48e8f2d1f43ad3a72
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-30 10:39:08 +0300

    update navbar buttons

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -7,7 +7,7 @@ const Navbar = ({ data }) => {
       <nav className="flex justify-center gap-x-7 gap-y-3 lg:gap-0 lg:space-x-7 -space-x-5 ">
         {data.items.map(({ title, link }, index) => (
           <Link href={link} key={index}>
-            <a className="text-blue-400 rounded-xl p-2 border border-green-500 text-base lg:text-xl lg:leading-9 no-underline font-normal hover:bg-green-100 hover:rounded-xl hover:text-red-400 focus:bg-red-400 focus:rounded-xl focus:text-white hover:border-red-300 focus:border-blue-300">
+            <a className="text-blue-400 rounded-xl p-2 border border-blue-900 shadow-sm shadow-slate-900 text-base lg:text-xl lg:leading-9 no-underline font-normal hover:bg-green-100 hover:rounded-xl hover:text-red-400 focus:bg-red-400 focus:rounded-xl focus:text-white hover:border-red-300 focus:border-blue-300">
               {title}
             </a>
           </Link>
