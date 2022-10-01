82efa2d19f0edc1464acbe5c32e2f7fa5db52791
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-01 20:17:16 +0300

    update grid structure

--- components/social/index.js
+++ components/social/index.js
@@ -1,12 +1,12 @@
 const Social = () => {
   return (
     <>
-      <div className="grid grid-cols-4 mt-8 md:flex-row border-t-2 pt-1">
+      <div className="grid grid-cols-4 mt-8 md:flex-row border-t-2 pt-2">
         <a
           href="https://www.linkedin.com/in/huseyinbattal42/"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
             <svg
@@ -25,7 +25,7 @@ const Social = () => {
           href="https://github.com/huseyinbattal"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
             <svg
@@ -44,7 +44,7 @@ const Social = () => {
           href="https://www.twitter.com/hsyn_bttl"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
             <svg
@@ -63,7 +63,7 @@ const Social = () => {
           href="https://www.instagram.com/hsyn_battal/"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#CC0099" class="bi bi-instagram" viewBox="0 0 16 16">
@@ -76,7 +76,7 @@ const Social = () => {
           href="https://www.facebook.com/hsynbattal/"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="dodgerblue" class="bi bi-facebook" viewBox="0 0 16 16">
@@ -89,7 +89,7 @@ const Social = () => {
           href="https://medium.com/@huseyinbattal42"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" class="bi bi-medium" viewBox="0 0 16 16">
@@ -101,7 +101,7 @@ const Social = () => {
           href="https://www.hackerrank.com/huseyinbattal42"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
           <svg height="50" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="1 1 582 486.999"><path d="M-1-1h582v402H-1z" fill="none"/><path d="M454.843 141.001c-13.019-22.417-172.832-115-198.859-115-26.019 0-185.895 92.351-198.84 115-12.947 22.649-13.019 207.358 0 230.009 13.018 22.639 172.839 114.989 198.84 114.989 26 0 185.841-92.466 198.851-114.999 13.007-22.533 13.016-207.583.008-229.999zM309.862 398.15c-3.559 0-36.756-32.137-34.141-34.762.781-.78 5.625-1.328 15.768-1.644 0-23.564.53-61.622.844-77.553.038-1.814-.395-3.081-.395-5.256h-71.812c0 6.379-.412 32.523 1.232 65.479.205 4.078-1.42 5.353-5.158 5.335-9.102-.025-18.211-.099-27.321-.071-3.683.009-5.274-1.374-5.157-5.488.826-30.043 2.66-75.488-.134-191.07v-2.849c-8.688-.314-14.717-.862-15.508-1.652-2.624-2.624 31.032-34.76 34.581-34.76 3.558 0 36.989 32.145 34.383 34.76-.782.781-7.098 1.338-15.067 1.652v2.84c-2.174 23.135-1.823 71.506-2.362 94.686h72.107c0-4.089.351-31.212-1.077-75.145-.091-3.047.853-4.646 3.781-4.672 9.945-.072 19.9-.117 29.855-.055 3.108.019 4.105 1.546 4.043 4.834-3.28 171.861-.594 159.867-.594 188.975 7.97.315 15.112.864 15.895 1.655 2.588 2.615-30.205 34.761-33.763 34.761z" fill="#2ec866"/></svg>
@@ -112,7 +112,7 @@ const Social = () => {
           href="https://www.youtube.com/channel/UCCg4uh_vbLJJ7-H58W0PmuQ"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
+          className="inline-flex justify-center rounded hover:bg-black/[.1] p-4 no-underline hover:no-underline hover:text-black"
         >
           <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="red" class="bi bi-youtube" viewBox="0 0 16 16">
