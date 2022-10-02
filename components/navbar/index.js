67f98c1f46acf37391d62fd13ca626d0c567d7da
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-02 09:59:34 +0300

    change css style

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,17 +1,31 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-
-const Navbar = ({ data }) => {
+const Navbar = () => {
+  const router = useRouter();
   return (
     <>
       <nav className="flex justify-center gap-x-7 gap-y-3 lg:gap-0 lg:space-x-7 -space-x-5 mb-5 ">
-        {data.items.map(({ title, link }, index) => (
-          <Link href={link} key={index}>
-            <a className="text-blue-400 rounded-xl p-2 border border-blue-900 shadow-sm shadow-slate-900 text-base lg:text-xl lg:leading-9 no-underline hover:bg-white hover:rounded-xl hover:text-red-400 focus:bg-red-400 focus:rounded-xl focus:text-white hover:border-red-300 focus:border-blue-300 w-24 text-center font-semibold">
-              {title}
-            </a>
-          </Link>
-        ))}
+        <Link href="/">
+          <a className={router.pathname == "/" ? "active" : "deActive"}>
+            Home
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className={router.pathname == "/projects" ? "active" : "deActive"}>
+            Projects
+          </a>
+        </Link>
+        <Link href="/skills">
+          <a className={router.pathname == "/skills" ? "active" : "deActive"}>
+            Skills
+          </a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname == "/about" ? "active" : "deActive"}>
+            About
+          </a>
+        </Link>
       </nav>
     </>
   );
