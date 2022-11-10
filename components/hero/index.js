ac63ae3154cf6db317ba72586f81475995b92273
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-10 16:05:07 +0300

    update hero section

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react"
 import Typed from "typed.js"
 
-const hero="../../public/images/hero.png"
+
 
 const Hero = () => {
   const el = useRef(null)
@@ -22,7 +22,7 @@ const Hero = () => {
   }, [])
   return (
     <>
-      <h1 className="text-3xl lg:text-5xl font-semibold my-5 text-red-500 text-center md:text-left"><img className="float-right" width="30px" src={hero} alt="hblogo" />Hüseyin BATTAL</h1>
+      <h1 className="text-3xl lg:text-5xl font-semibold my-5 text-red-500 text-center md:text-left"><img className="float-right" width="30px" src="https://reactdeveloper.site/logobg.png" alt="hblogo" />Hüseyin BATTAL</h1>
       
       <h2 className="text-xl lg:text-2xl md:ml-1 font-normal mb-5 h-16 md:h-auto text-pink-400 text-center md:text-left">
         <span className="text-blue-600 italic font-mono" ref={el}></span>
