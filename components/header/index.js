556567a4c9b32a51b2f0e1783072ef43d3969282
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-10 21:17:46 +0300

    update navbar

--- components/header/index.js
+++ components/header/index.js
@@ -5,8 +5,8 @@ import data from "@/public/data/header.json"
 const Header = () => {
   return (
     <>
-      <header className="sticky top-0 bg-gray-200 w-full p-5 z-10">
-        <div className="lg:flex items-center justify-center xl:justify-between">
+      <header className="sticky top-0 bg-gray-200 w-full p-1 z-10">
+        <div className="lg:flex items-center justify-center xl:justify-between mt-4">
           <Brand />
           <Navbar data={data} />
         </div>
