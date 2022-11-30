93597ac828020ceddd61eeea0511b73df09f95f1
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-30 11:33:37 +0300

    update logo src

--- components/brand/index.js
+++ components/brand/index.js
@@ -1,5 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
-const logo = "https://reactdeveloper.site/giphy.gif";
+// const logo = "https://reactdeveloper.site/giphy.gif";
+import reactLogo from "../../public/images/giphy.gif"
+import hbLogo from "../../public/images/logobg.png"
+
 
 const Brand = () => {
   return (
@@ -7,9 +11,10 @@ const Brand = () => {
       <div className="flex justify-center items-center mb-4 xl:ml-5">
         <Link href="/" aria-label="Home Page">
           <figure className="p-1 cursor-pointer transition hover:scale-125  duration-300">
-            <img
-              width="60px"
-              src="https://reactdeveloper.site/logobg.png"
+            <Image
+              width={50}
+              height={50}
+              src={hbLogo}
               alt="HB Logo"
             />
           </figure>
@@ -24,7 +29,7 @@ const Brand = () => {
               href="https://reactjs.org/"
               target="_blank"
             >
-              <img className="text-white" width="30px" src={logo} alt="logo" />
+              <Image className="text-white" width={30} height={30} src={reactLogo} alt="logo" />
             </a>
             <span style={{ color: "rgb(47,196,99)" }}>{"/>"}</span>
           </span>
