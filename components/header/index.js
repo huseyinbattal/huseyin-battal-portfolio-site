2e9ff1595611fca46cde9db936032019a5f1cc41
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-10 15:29:46 +0300

    fix bug

--- components/header/index.js
+++ components/header/index.js
@@ -5,8 +5,8 @@ import data from "@/public/data/header.json"
 const Header = () => {
   return (
     <>
-      <header className="sticky top-0 bg-slate-100 w-full p-3 z-10 mt-4 ">
-        <div className="lg:flex items-center justify-center xl:justify-between mt-4">
+      <header className="sticky top-0 bg-slate-200 w-full p-3 z-10 mt-4 ">
+        <div className="lg:flex items-center justify-between mx-12 xl:justify-between mt-4">
           <Brand />
           <Navbar data={data} />
         </div>
